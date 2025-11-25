9c8015a5a0ec1ca70d20d514356a7844fe14c60a
Kevin Rosengren <rosenkev4@gmail.com>
2025-11-25 01:33:04 -0600

    Added New method to database.go file to get connection to the database

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -2,6 +2,10 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
+	appconfig "northwind-api/internal/config"
+
+	"github.com/rs/zerolog/log"
 )
 
 type DB struct {
@@ -9,7 +13,22 @@ type DB struct {
 }
 
 // Create new database connection
-// func New(cfg *appconfig.Config) (*DB, error) {
-// 	// Get the database URL
-// 	databaseURL, err := cfg.
-// }
+func New(cfg *appconfig.Config) (*DB, error) {
+	// Get the database URL
+	databaseURL, err := cfg.GetDatabaseURL()
+	if err != nil {
+		return nil, fmt.Errorf("could not get the database url: %w", err)
+	}
+
+	db, err := sql.Open("postgres", databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	log.Info().Msg("Database successfully connected!")
+	return &DB{DB: db}, nil
+}
