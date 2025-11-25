64c7159730c1384901a3f838d23d87506b8b7e36
Kevin Rosengren <rosenkev4@gmail.com>
2025-11-25 04:25:24 -0600

    Added handler func to get a category by its ID

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -6,6 +6,7 @@ import (
 	appconfig "northwind-api/internal/config"
 	"northwind-api/internal/repository"
 
+	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 )
 
@@ -42,6 +43,7 @@ func writeErrorResponse(w http.ResponseWriter, status int, message string) {
 	writeJSONResponse(w, status, ErrorResponse{Error: message})
 }
 
+// Handler to get all categories
 func (h *Handler) GetCategories(w http.ResponseWriter, r *http.Request) {
 	log.Info().Msg("GET /categories - Getting all of the categories")
 
@@ -54,4 +56,28 @@ func (h *Handler) GetCategories(w http.ResponseWriter, r *http.Request) {
 
 	log.Info().Int("count", len(categories)).Msg("Successfully retrieved categories")
 	writeJSONResponse(w, http.StatusOK, categories)
+}
+
+// Handler to get category by its ID
+func (h *Handler) GetCategoryById(w http.ResponseWriter, r *http.Request) {
+	log.Info().Msg("GET /api/categories/{ID} - Getting category by ID")
+
+	// Extract the ID from the URL path
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+
+	category, err := h.db.GetCategoryById(idStr)
+	if err != nil {
+		if err.Error() == "category not found" {
+			log.Warn().Str("ID", idStr).Msg("Category not found")
+			writeErrorResponse(w, http.StatusInternalServerError, "Category not found")
+			return
+		}
+		log.Error().Err(err).Msg("Error getting categories")
+		writeErrorResponse(w, http.StatusInternalServerError, "Failed to get that category")
+		return
+	}
+
+	log.Info().Str("ID", idStr).Msg("Successfully retrieved the category")
+	writeJSONResponse(w, http.StatusOK, category)
 }
