2cf70c969261773e21ad4f6f143f245d0c29a9e0
Kevin Rosengren <rosenkev4@gmail.com>
2025-11-25 02:09:57 -0600

    Set up the handler.go file with a method to create new instance, an error resposne, and a JSON response writer func

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,5 +1,23 @@
 package handler
 
-// type Handler struct {
-// 	db
-// }
+import (
+	appconfig "northwind-api/internal/config"
+	"northwind-api/internal/repository"
+)
+
+type Handler struct {
+	db     *repository.DB
+	config *appconfig.Config
+}
+
+// Create a new instance of handler
+func New(db *repository.DB, cfg *appconfig.Config) *Handler {
+	return &Handler{
+		db:     db,
+		config: cfg,
+	}
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
